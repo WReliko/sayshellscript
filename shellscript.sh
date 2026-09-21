b1f9b5112ae855805a53b1787282c99039d725f8
WReliko <william_chang@horacemann.org>
2026-09-21 00:26:10 -0400

    Add files via upload

---
+++ shellscript.sh
@@ -0,0 +1,7 @@
+say "Welcome to my shell."
+echo "Input your name: "
+read -r yourname
+say "Hello, "$yourname"."
+echo "What is your favorite flavor of ice cream?"
+read -r ic
+say ""$ic" is a nice flavor."
